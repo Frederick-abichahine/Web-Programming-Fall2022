38c7aa07f71e53f5765f348188125a2a0615b485
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-09-17 22:42:59 +0300

    Updating game.js by adding appropriate flags & starting conditions

--- Web_Assignment_2/game.js
+++ Web_Assignment_2/game.js
@@ -1,35 +1,53 @@
 
 /*
-############################
-    Declaring variables
-############################
+#################################################
+    Declaring variables (& initializing some)
+#################################################
 */
 
-let bound      //array used to store all the boundaries
-let begin      //
+let boundaries        //array used to store all the boundaries
+let begin             //variable to ensure that the user wants to start the game
+let flag = false      //this flag will indicate if the user hovered over "S" or not in order to properly start the game
 
 /*
-############################
-            Main
-############################
+#################################################
+                      Main
+#################################################
 */
 
 window.onload = () => {
-    // begin = document.getElementById("start")
-    bound = document.getElementsByClassName("boundary")
-    for (let i = 0; i<bound.length; i++){
-        bound[i].onmouseover = touchBoundary
+
+    begin = document.getElementById("start")
+    begin.onmouseover = touchStart                                 //this will enter the function touchStart whent the player hovers over "S" to properly start the game
+    boundaries = document.getElementsByClassName("boundary")
+
+    for (let i = 0; i<boundaries.length; i++){
+        boundaries[i].onmouseover = touchBoundary                  //this will ensure that the borders turn red if the player hovers over them
     }
 }
 
 /*
-############################
-       Other Functions
-############################
+#################################################
+                Other Functions
+#################################################
 */
 
 let touchBoundary = () => {
-    for (let i = 0; i<bound.length - 1; i++){
-        bound[i].style.backgroundColor = "red"
+
+    if(flag){ //this will check if the user hovered over "S" first to ensure that the borders turn red ONLY AFTER the user decides to start the game
+
+        for (let i = 0; i<boundaries.length - 1; i++){
+            //boundaries[i].style.backgroundColor = "#ff8888"
+            boundaries[i].className += " youlose" //This will override the original boundary color using the present youlose color in the CSS file
+        }
     }
+}
+
+let touchStart = () => {
+
+    flag = true //to ensure that the user started the game and only now will the borders turn red after hovering over "S"
+}
+
+let touchEnd = () => {
+
 }
