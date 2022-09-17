796466d184bb3e768d48048e502cf73ffccc55ee
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-09-17 22:07:25 +0300

    Re-constructing JS backbone

--- Web_Assignment_2/game.js
+++ Web_Assignment_2/game.js
@@ -1,15 +1,35 @@
 
-let bound
+/*
+############################
+    Declaring variables
+############################
+*/
+
+let bound      //array used to store all the boundaries
+let begin      //
+
+/*
+############################
+            Main
+############################
+*/
 
 window.onload = () => {
+    // begin = document.getElementById("start")
     bound = document.getElementsByClassName("boundary")
     for (let i = 0; i<bound.length; i++){
-        bound[i].onmouseover = touchBoundary()
+        bound[i].onmouseover = touchBoundary
     }
 }
 
+/*
+############################
+       Other Functions
+############################
+*/
+
 let touchBoundary = () => {
-    for (let i = 0; i<bound.length; i++){
-        bound[i].style.color = "red"
+    for (let i = 0; i<bound.length - 1; i++){
+        bound[i].style.backgroundColor = "red"
     }
 }
