bb1990bc84e67d6e0ff4300b4b6639d248825dd9
Frederick A. Chahine <Frederickabichahine@gmail.com>
2022-11-02 19:36:50 +0200

    Adding <> Lectures 19, 20 & 21 connection.php

---
+++ Web_Lectures/Web_programming_Lec19_20_&_21/connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "webdbb";
+
+$mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
+
+#now we have a conection between the database and php file
